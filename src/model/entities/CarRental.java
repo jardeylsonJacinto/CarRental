a7ea122f6896ea9fc8e23aa2f7f0c43c1b855d53
Jardeylson Jacinto <jardeylsong.m@gmail.com>
2024-12-16 12:33:01 -0300

    feat: create class CarRental

---
+++ src/model/entities/CarRental.java
@@ -0,0 +1,21 @@
+package model.entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	
+	private Vehicle vehicle;
+	private Invoice invoice;
+	
+	public CarRental() {}
+
+	public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle) {
+		this.start = start;
+		this.finish = finish;
+		this.vehicle = vehicle;
+	}
+	
+	
+}
